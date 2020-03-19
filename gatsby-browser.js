a2e17ffb9c791a0a5a98574f7ea449a4076273c9
Kunal Dewan <kunalx011@gmail.com>
2020-03-19 21:46:45 +0530

    Adding scrollTo 00 on client render

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,6 @@
 exports.onInitialClientRender = () => {
+  window.scrollTo(0, 0);
   setTimeout(function() {
     document.getElementById("___loader").style.display = "none"
   }, 200)
 }
-
-
-exports.onRouteUpdate = () => {
-  if (typeof window !== `undefined`) { window.scrollTo(0, 0)}
-}
-
-exports.shouldUpdateScroll = (args) => {
-  return false;
-};
