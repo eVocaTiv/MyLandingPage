444a96c640869db44eba6fd40c41e763d6e414b1
Kunal Dewan <kunalx011@gmail.com>
2020-03-19 21:08:36 +0530

    Adding scroll bug fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,12 @@ exports.onInitialClientRender = () => {
     document.getElementById("___loader").style.display = "none"
   }, 200)
 }
+
+
+exports.onRouteUpdate = () => {
+  if (typeof window !== `undefined`) { window.scrollTo(0, 0)}
+}
+
+exports.shouldUpdateScroll = (args) => {
+  return false;
+};
