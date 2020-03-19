ea4da40c4d9d3a1e436b4a145f1b22deff2c9692
Kunal Dewan <kunalx011@gmail.com>
2020-03-19 05:33:41 +0530

    Parallax bug 1

--- src/appContent/App.js
+++ src/appContent/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <ScrollAnimation duration={1.5} animateOnce animateIn="zoomIn">
+        <ScrollAnimation duration={1.5}  animateIn="zoomIn">
           <TopLayerContainer />
           <ScrollAnimation animateIn="zoomIn">
             <VisionContainer />
