4ae9ebfa9c7e96afc8ab255929c9a7c09f7a2561
Kunal Dewan <kunalx011@gmail.com>
2020-03-30 00:06:10 +0530

    Added model in top layer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from '@colors/theme';
-import {
-   TopLayerContainer,
-} from '@containers';
+import { TopLayerContainer } from '@containers';
+import App from '@components/3DModel/App.js';
+import { NavBar } from '@components';
+import styled from 'styled-components';
+
+const NavBarContainer = styled.div`
+  width: 100%;
+`;
 
 class LandingPage extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-          <TopLayerContainer />
+        <NavBarContainer>
+          <NavBar />
+        </NavBarContainer>
+        <App />
       </ThemeProvider>
     );
   }
