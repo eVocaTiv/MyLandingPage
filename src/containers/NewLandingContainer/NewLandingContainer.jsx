09bd127f02bb844fe8b566235519a6f66f156180
Kunal Dewan <kunalx011@gmail.com>
2020-04-03 05:33:25 +0530

    Scroll on landing

--- src/containers/NewLandingContainer/NewLandingContainer.jsx
+++ src/containers/NewLandingContainer/NewLandingContainer.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import NewLandingContainerBGImage from '@components/Images/NewLandingContainerBGImage';
+import styled from 'styled-components';
+
+const fullScreenDiv = styled.div`
+  height: 100vh;
+  overflow: hidden;
+`;
 
 class NewLandingContainer extends Component {
   render() {
