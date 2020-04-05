5352387f8f760f6f4d369715a31ce222a6d44f6d
Kunal Dewan <kunalx011@gmail.com>
2020-04-05 12:39:07 +0530

    Removed unused styles

--- src/containers/NewLandingContainer/NewLandingContainer.jsx
+++ src/containers/NewLandingContainer/NewLandingContainer.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import NewLandingContainerBGImage from '@components/Images/NewLandingContainerBGImage';
-import styled from 'styled-components';
 import '@styles/commonStyles.css';
 import '@styles/newLandingStyles.css';
 import 'animate.css/animate.min.css';
