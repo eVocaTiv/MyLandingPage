130efe3f5bf7ea3dcb72d2628d95f5e4611d36c7
Kunal Dewan <kunalx011@gmail.com>
2020-04-06 17:38:40 +0530

    maxHeight lt. to 1000 vh

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,10 +8,10 @@ exports.onRouteUpdate = () => {
   document.getElementById('___loader').style.display = 'flex';
   setTimeout(function() {
     document.getElementById('___gatsby').style.visibility = 'visible';
-    document.getElementById('___gatsby').style.maxHeight = '250vh';
+    document.getElementById('___gatsby').style.maxHeight = '1000vh';
     document.getElementById('___gatsby').style.opacity = 1;
     document.getElementById('___gatsby').style.overflowY = 'auto';
-    document.getElementById('___body').style.maxHeight = '250vh';
+    document.getElementById('___body').style.maxHeight = '1000vh';
     document.getElementById('___body').style.overflowY = 'auto';
     document.getElementById('___loader').style.display = 'none';
   }, 100);
