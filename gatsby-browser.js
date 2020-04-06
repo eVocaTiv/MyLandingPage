b276b7e639970bab0b3c2fd5627c75e5c21c890a
Kunal Dewan <kunalx011@gmail.com>
2020-04-06 17:18:08 +0530

    Body maxheight

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,7 @@ exports.onRouteUpdate = () => {
   document.getElementById('___gatsby').style.visibility = 'hidden';
   document.getElementById('___gatsby').style.maxHeight = 0;
   document.getElementById('___gatsby').style.overflowY = 'hidden';
+  document.getElementById('___body').style.maxHeight = '100vh';
   document.getElementById('___body').style.overflowY = 'hidden';
   document.getElementById('___loader').style.display = 'flex';
   setTimeout(function() {
@@ -10,7 +11,8 @@ exports.onRouteUpdate = () => {
     document.getElementById('___gatsby').style.maxHeight = '250vh';
     document.getElementById('___gatsby').style.opacity = 1;
     document.getElementById('___gatsby').style.overflowY = 'auto';
-    document.getElementById('___loader').style.display = 'none';
+    document.getElementById('___body').style.maxHeight = '250vh';
     document.getElementById('___body').style.overflowY = 'auto';
+    document.getElementById('___loader').style.display = 'none';
   }, 100);
 };
