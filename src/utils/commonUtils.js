e3ddb7c0aef766cfb27ff4240c23900ba4572a0c
Kunal Dewan <kunalx011@gmail.com>
2020-04-06 17:48:27 +0530

    Updated onNavigate scroll bug fix

--- src/utils/commonUtils.js
+++ src/utils/commonUtils.js
@@ -13,15 +13,24 @@ const processNavigation = (currentSection, URL) => {
   if (URL === '/') {
     // if not at home already.
     if (currentSection.indexOf(homeString) === -1) {
-      document.getElementById('___gatsby').style.opacity = 0;
+      document.getElementById('___gatsby').style.visibility = 'hidden';
+      document.getElementById('___gatsby').style.maxHeight = 0;
+      document.getElementById('___gatsby').style.overflowY = 'hidden';
+      document.getElementById('___body').style.maxHeight = '100vh';
+      document.getElementById('___body').style.overflowY = 'hidden';
       document.getElementById('___loader').style.display = 'flex';
       navigate('/');
     }
   } else {
     // navigate from other sections
     if (`/${currentSection}/` != URL) {
-      document.getElementById('___gatsby').style.opacity = 0;
-      document.getElementById('___loader').style.display = 'flex';
+      document.getElementById('___gatsby').style.visibility = 'visible';
+      document.getElementById('___gatsby').style.maxHeight = '1000vh';
+      document.getElementById('___gatsby').style.opacity = 1;
+      document.getElementById('___gatsby').style.overflowY = 'auto';
+      document.getElementById('___body').style.maxHeight = '1000vh';
+      document.getElementById('___body').style.overflowY = 'auto';
+      document.getElementById('___loader').style.display = 'none';
       navigate(URL);
     }
   }
