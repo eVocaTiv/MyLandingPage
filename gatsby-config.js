527c35515b2e506f34ff6c4efb213099c1b1a98e
Kunal Dewan <kunalx011@gmail.com>
2020-04-12 13:39:07 +0530

    Added 404 page & removed unused layout.

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,6 @@ module.exports = {
           '@components': 'src/components',
           '@containers': 'src/containers',
           '@constants': 'src/constants',
-          '@layouts': 'src/layouts',
           '@pages': 'src/pages',
           '@sass': 'src/sass',
           '@templates': 'src/templates',
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,38 @@
-import React from "react"
+import React from 'react';
+import styled from 'styled-components';
+import SEO from '@components/seo';
+import Button from '@material-ui/core/Button';
+import { navigate } from 'gatsby';
 
-// import Layout from "@components/layout"
-import SEO from "@components/seo"
+const NotFoundContainer = styled.div`
+  width: 100%;
+  height: 90vh !important;
+  background-color: black !important;
+  text-align: center;
+`;
+
+const StyledButton = styled(Button)`
+  &:hover {
+    transform: scale(1.5);
+  }
+  color: #803966 !important;
+  transition: transform 0.5s ease;
+`;
 
 const NotFoundPage = () => (
-  // <Layout>
-  <h1> 404 </h1>
-    // <SEO title="404: Not found" />
-    // <h1>NOT FOUND</h1>
-    // <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  // </Layout>
-)
+  <NotFoundContainer>
+    <SEO title="404: Not found" />
+    <h1 style={{ color: '#00C2CB', margin: '10%' }}>
+      This page doesn't exist!
+    </h1>
+    <StyledButton
+      onClick={() => navigate('/')}
+      color="inherit"
+      className="nav-button"
+    >
+      <h2>Home</h2>
+    </StyledButton>
+  </NotFoundContainer>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
